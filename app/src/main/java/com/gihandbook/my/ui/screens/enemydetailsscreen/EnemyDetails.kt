3e6a30caea37556a0b979724fbacba4eadc44891
Veronika Kapitanova <brigmore.w@gmail.com>
2022-10-06 20:03:16 +0600

    [+] add back button on enemy screen

--- app/src/main/java/com/gihandbook/my/ui/screens/enemydetailsscreen/EnemyDetails.kt
+++ app/src/main/java/com/gihandbook/my/ui/screens/enemydetailsscreen/EnemyDetails.kt
@@ -1,14 +1,19 @@
 package com.gihandbook.my.ui.screens.enemydetailsscreen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -47,92 +52,105 @@ fun EnemyDetailsScreen(
 
 @Composable
 fun ShowEnemyDetails(enemy: EnemyUIModel, onBackButtonClick: () -> Unit) {
-    Column(
-        modifier = Modifier
-            .padding(horizontal = 16.dp)
-            .clip(RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp))
-            .fillMaxWidth()
-    ) {
-        AsyncImage(
-            model = enemy.imageUrl,
-            contentDescription = null,
+    Box(modifier = Modifier.fillMaxSize()) {
+        IconButton(
+            onClick = onBackButtonClick, modifier = Modifier
+                .padding(8.dp)
+                .align(Alignment.TopEnd)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_close_svgrepo_com),
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(MaterialTheme.colors.primary)
+            )
+        }
+        Column(
             modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .clip(RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp))
                 .fillMaxWidth()
-                .sizeIn(maxHeight = 350.dp)
-        )
-        IconWithText(title = enemy.name, icon = null)
-        CharacterSmallInfoRow(
-            content = {
-                InfoItemWithIcon(
-                    icon = R.drawable.ic_map_svgrepo_com,
-                    itemType = stringResource(id = R.string.character_region_title),
-                    itemValue = enemy.region!!
-                )
-
-                InfoItemWithIcon(
-                    icon = R.drawable.ic_user_svgrepo_com__1_,
-                    itemType = "Type",
-                    itemValue = enemy.type
-                )
-
-                InfoItemWithIcon(
-                    icon = R.drawable.ic_mora,
-                    itemType = stringResource(id = R.string.enemy_mora_gained),
-                    itemValue = enemy.moraGained
-                )
-            })
-
-        TextBlock(
-            title = stringResource(id = R.string.character_description_title),
-            text = enemy.description
-        )
-
-        TextBlock(title = stringResource(id = R.string.enemy_materials_title))
-        enemy.elements?.let {
-            ExpandableList(
-                title = stringResource(id = R.string.enemy_vision_title),
-                itemsList = it,
-                rowContent = {
-                    it.forEach { element -> ExpandableListIcon(element.iconUrl) }
-                },
-                bodyContent = { element ->
-                    ElementExpandableListItem(element = element as Element)
-                }
+        ) {
+            AsyncImage(
+                model = enemy.imageUrl,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .sizeIn(maxHeight = 350.dp)
             )
-        }
+            IconWithText(title = enemy.name, icon = null)
+            CharacterSmallInfoRow(
+                content = {
+                    InfoItemWithIcon(
+                        icon = R.drawable.ic_map_svgrepo_com,
+                        itemType = stringResource(id = R.string.character_region_title),
+                        itemValue = enemy.region!!
+                    )
 
-        enemy.drops?.let {
-            ExpandableList(
-                title = stringResource(id = R.string.enemy_drops_title),
-                itemsList = it,
-                rowContent = {
-                    enemy.drops.forEach { element ->
-                        ExpandableListIcon(
-                            element.imageUrl,
-                            element.imageUrlOnError
-                        )
+                    InfoItemWithIcon(
+                        icon = R.drawable.ic_user_svgrepo_com__1_,
+                        itemType = "Type",
+                        itemValue = enemy.type
+                    )
+
+                    InfoItemWithIcon(
+                        icon = R.drawable.ic_mora,
+                        itemType = stringResource(id = R.string.enemy_mora_gained),
+                        itemValue = enemy.moraGained
+                    )
+                })
+
+            TextBlock(
+                title = stringResource(id = R.string.character_description_title),
+                text = enemy.description
+            )
+
+            TextBlock(title = stringResource(id = R.string.enemy_materials_title))
+            enemy.elements?.let {
+                ExpandableList(
+                    title = stringResource(id = R.string.enemy_vision_title),
+                    itemsList = it,
+                    rowContent = {
+                        it.forEach { element -> ExpandableListIcon(element.iconUrl) }
+                    },
+                    bodyContent = { element ->
+                        ElementExpandableListItem(element = element as Element)
                     }
-                },
-                bodyContent = { element ->
-                    DropsExpandableListItem(element as DropUI)
-                }
-            )
-        }
-        enemy.artifacts?.let {
-            ExpandableList(
-                title = stringResource(id = R.string.enemy_artifact_title),
-                itemsList = it,
-                rowContent = {
-                    enemy.artifacts.forEach { element ->
-                        ExpandableListIcon(
-                            element.imageUrl
-                        )
+                )
+            }
+
+            enemy.drops?.let {
+                ExpandableList(
+                    title = stringResource(id = R.string.enemy_drops_title),
+                    itemsList = it,
+                    rowContent = {
+                        enemy.drops.forEach { element ->
+                            ExpandableListIcon(
+                                element.imageUrl,
+                                element.imageUrlOnError
+                            )
+                        }
+                    },
+                    bodyContent = { element ->
+                        DropsExpandableListItem(element as DropUI)
                     }
-                },
-                bodyContent = { element ->
-                    ArtifactsExpandableListItem(element as ArtifactUI)
-                }
-            )
+                )
+            }
+            enemy.artifacts?.let {
+                ExpandableList(
+                    title = stringResource(id = R.string.enemy_artifact_title),
+                    itemsList = it,
+                    rowContent = {
+                        enemy.artifacts.forEach { element ->
+                            ExpandableListIcon(
+                                element.imageUrl
+                            )
+                        }
+                    },
+                    bodyContent = { element ->
+                        ArtifactsExpandableListItem(element as ArtifactUI)
+                    }
+                )
+            }
         }
     }
 }
